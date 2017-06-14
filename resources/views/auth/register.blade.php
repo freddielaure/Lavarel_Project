1171625ad38408e782ab2d728e557fb2b9c7f714
Laure Tchagam <freddielaure@p058165.nat-gm.FH-Koeln.DE>
2017-06-14 14:52:51 +0200

    update

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -38,6 +38,13 @@
                         </div>
 
 
+
+
+
+
+
+
+
                         <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
                             <label for="email" class="col-md-4 control-label">E-Mail Address</label>
 
