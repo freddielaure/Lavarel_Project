c4e363f7866c5c22dcf0f8e7eabd4df7e341bd7e
ALli Pierre <P.Yotti@atlantic-bonn.de>
2017-06-14 12:59:28 +0200

    Remove Bad Coding

--- app/Http/Controllers/ItemCRUDController.php
+++ app/Http/Controllers/ItemCRUDController.php
@@ -73,7 +73,7 @@ class ItemCRUDController extends Controller
     */
    public function show($produkte_id)
    {
-       $item = DB::table('produkte')->join('lagerorte', 'lagerorte.lagerorte_id', '=', 'produkte.produkte_id')->get();
+       $item = DB::table('produkte')->join('lagerorte', 'lagerorte.lagerorte_id', '=', 'produkte.produkte_id','produkte.produkte_id', '=', $produkte_id)->get();
       //$item = produkte::with('lagerorte')->get();
        return view('show',compact('item'));
    }
